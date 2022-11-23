23db198cf3ad09a9f00d294754f61a2f16ba3d10
rita-aris <ritaaris@yandex.ru>
2022-11-24 01:04:53 +0400

    create lecture-1.js

---
+++ Js-tasks/lecture-1/lecture-1.js
@@ -0,0 +1,21 @@
+const myName = "Margo";
+let myAge = 31;
+let myWeight = 45;
+let isAdult = true;
+let isDriver = false;
+let favoriteMusic = "rock";
+let eyeColor = "grey";
+let myAddress = undefined;
+
+console.log(String(myName), Number(myName), Boolean(myName));
+console.log(String(myAge), Number(myAge), Boolean(myAge));
+console.log(String(myWeight), Number(myWeight), Boolean(myWeight));
+console.log(String(isAdult), Number(isAdult), Boolean(isAdult));
+console.log(String(isDriver), Number(isDriver), Boolean(isDriver));
+console.log(
+  String(favoriteMusic),
+  Number(favoriteMusic),
+  Boolean(favoriteMusic)
+);
+console.log(String(eyeColor), Number(eyeColor), Boolean(eyeColor));
+console.log(String(myAddress), Number(myAddress), Boolean(myAddress));
