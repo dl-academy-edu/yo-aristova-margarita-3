ce749057cf71c6d52ad6f08da237eafa33ddbe05
rita-aris <83461001+rita-aris@users.noreply.github.com>
2022-11-24 13:55:46 +0400

    fix homework 1

--- Js-tasks/lecture-1/lecture-1.js
+++ Js-tasks/lecture-1/lecture-1.js
@@ -1,21 +1,20 @@
 const myName = "Margo";
-let myAge = 31;
-let myWeight = 45;
+let myAge = 45;
+let bigInt = 123456789n;
 let isAdult = true;
-let isDriver = false;
-let favoriteMusic = "rock";
-let eyeColor = "grey";
-let myAddress = undefined;
+let list = null;
+let myCountry = undefined;
+let id = Symbol("id");
+let myAddress = {
+  city: "Tolyatti",
+  district: false,
+};
 
 console.log(String(myName), Number(myName), Boolean(myName));
 console.log(String(myAge), Number(myAge), Boolean(myAge));
-console.log(String(myWeight), Number(myWeight), Boolean(myWeight));
+console.log(String(bigInt), Number(bigInt), Boolean(bigInt));
 console.log(String(isAdult), Number(isAdult), Boolean(isAdult));
-console.log(String(isDriver), Number(isDriver), Boolean(isDriver));
-console.log(
-  String(favoriteMusic),
-  Number(favoriteMusic),
-  Boolean(favoriteMusic)
-);
-console.log(String(eyeColor), Number(eyeColor), Boolean(eyeColor));
+console.log(String(list), Number(list), Boolean(list));
+console.log(String(myCountry), Number(myCountry), Boolean(myCountry));
 console.log(String(myAddress), Number(myAddress), Boolean(myAddress));
+console.log(String(id), Number(id), Boolean(id));
