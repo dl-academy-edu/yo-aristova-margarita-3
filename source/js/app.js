58e70a0e3f188e2381faa25ab691f6fc1ce1ec5e
rita-aris <ritaaris@yandex.ru>
2022-12-11 19:46:40 +0400

    added sign in modal

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,32 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
+  const signInButton = document.querySelector(".sign-in-button");
+  const signInModal = document.querySelector(".sign-in");
+  const input = document.querySelector(".form__input");
+  const closeButton = document.querySelector(".sign-in__close");
+  const overlay = document.querySelector(".popup__overlay");
 
+  signInButton.addEventListener("click", function () {
+    signInModal.classList.toggle("sign-in--visible");
+    input.focus();
+  });
+
+  closeButton.addEventListener("click", function () {
+    signInModal.classList.toggle("sign-in--visible");
+    signInButton.focus();
+  });
+
+  overlay.addEventListener("click", function () {
+    signInModal.classList.toggle("sign-in--visible");
+    signInButton.focus();
+  });
+
+  window.addEventListener("keydown", function (event) {
+    if (
+      event.code === "Escape" &&
+      popup.classList.contains("sign-in--visible")
+    ) {
+      signInModal.classList.toggle("sign-in--visible");
+      signInButton.focus();
+    }
+  });
 });
