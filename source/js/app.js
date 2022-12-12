362f7e70b0f0457ec54c313d756fce34b82692df
rita-aris <83461001+rita-aris@users.noreply.github.com>
2022-12-12 14:43:06 +0400

    added responsive for modal

--- source/js/app.js
+++ source/js/app.js
@@ -1,32 +1,28 @@
 window.addEventListener("DOMContentLoaded", function () {
-  const signInButton = document.querySelector(".sign-in-button");
-  const signInModal = document.querySelector(".sign-in");
-  const input = document.querySelector(".form__input");
-  const closeButton = document.querySelector(".sign-in__close");
-  const overlay = document.querySelector(".popup__overlay");
-
-  signInButton.addEventListener("click", function () {
-    signInModal.classList.toggle("sign-in--visible");
-    input.focus();
-  });
-
-  closeButton.addEventListener("click", function () {
-    signInModal.classList.toggle("sign-in--visible");
-    signInButton.focus();
-  });
-
-  overlay.addEventListener("click", function () {
-    signInModal.classList.toggle("sign-in--visible");
-    signInButton.focus();
-  });
-
-  window.addEventListener("keydown", function (event) {
-    if (
-      event.code === "Escape" &&
-      popup.classList.contains("sign-in--visible")
-    ) {
-      signInModal.classList.toggle("sign-in--visible");
-      signInButton.focus();
-    }
-  });
+  // const signInButton = document.querySelector(".sign-in-button");
+  // const signInModal = document.querySelector(".sign-in");
+  // const input = document.querySelector(".form__input");
+  // const closeButton = document.querySelector(".modal__close");
+  // const overlay = document.querySelector(".modal__overlay");
+  // signInButton.addEventListener("click", function () {
+  //   signInModal.classList.toggle("sign-in--visible");
+  //   input.focus();
+  // });
+  // closeButton.addEventListener("click", function () {
+  //   signInModal.classList.toggle("sign-in--visible");
+  //   signInButton.focus();
+  // });
+  // overlay.addEventListener("click", function () {
+  //   signInModal.classList.toggle("sign-in--visible");
+  //   signInButton.focus();
+  // });
+  // window.addEventListener("keydown", function (event) {
+  //   if (
+  //     event.code === "Escape" &&
+  //     signInModal.classList.contains("sign-in--visible")
+  //   ) {
+  //     signInModal.classList.toggle("sign-in--visible");
+  //     signInButton.focus();
+  //   }
+  // });
 });
