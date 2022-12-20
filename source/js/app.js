ada2ee434d4e13ca3b6538e1549f431d6099ab38
rita-aris <ritaaris@yandex.ru>
2022-12-20 17:46:09 +0400

    added simple functionality for modal windows

--- source/js/app.js
+++ source/js/app.js
@@ -1,40 +1,58 @@
 window.addEventListener("DOMContentLoaded", function () {
-  // const signInButton = document.querySelector(".sign-in-button");
-  // const signInModal = document.querySelector(".sign-in");
-  // const input = document.querySelector(".form__input");
-  // const closeButton = document.querySelector(".modal__close");
-  // const overlay = document.querySelector(".modal__overlay");
-  // signInButton.addEventListener("click", function () {
-  //   signInModal.classList.toggle("sign-in--visible");
-  //   input.focus();
-  // });
-  // closeButton.addEventListener("click", function () {
-  //   signInModal.classList.toggle("sign-in--visible");
-  //   signInButton.focus();
-  // });
-  // overlay.addEventListener("click", function () {
-  //   signInModal.classList.toggle("sign-in--visible");
-  //   signInButton.focus();
-  // });
-  // window.addEventListener("keydown", function (event) {
-  //   if (
-  //     event.code === "Escape" &&
-  //     signInModal.classList.contains("sign-in--visible")
-  //   ) {
-  //     signInModal.classList.toggle("sign-in--visible");
-  //     signInButton.focus();
-  //   }
-  // });
+  const signInButton = document.querySelector(".sign-in-button");
+  const signInModal = document.querySelector(".sign-in");
+  const registerButton = document.querySelector(".register-button");
+  const registerModal = document.querySelector(".register");
+  const sendMessageModal = this.document.querySelector(".send-message");
+  const sendMessageButton = this.document.querySelector(".send-message-button");
+  const input = document.querySelector(".form__input");
+  const signCloseButton = document.querySelector(".sign-in__close");
+  const registerCloseButton = document.querySelector(".register__close");
+  const sendMessageCloseButton = this.document.querySelector(
+    ".send-message__close"
+  );
+
+  signInButton.addEventListener("click", () => {
+    signInModal.classList.add("sign-in--visible");
+    input.focus();
+  });
+
+  signCloseButton.addEventListener("click", function () {
+    signInModal.classList.remove("sign-in--visible");
+    signInButton.focus();
+  });
+
+  registerButton.addEventListener("click", () => {
+    registerModal.classList.add("register--visible");
+    input.focus();
+  });
+
+  registerCloseButton.addEventListener("click", function () {
+    registerModal.classList.remove("register--visible");
+    registerButton.focus();
+  });
+
+  sendMessageButton.addEventListener("click", () => {
+    sendMessageModal.classList.add("send-message--visible");
+    input.focus();
+  });
+
+  sendMessageCloseButton.addEventListener("click", function () {
+    sendMessageModal.classList.remove("send-message--visible");
+    sendMessageButton.focus();
+  });
+
+  // console.log([...this.document.querySelectorAll(".modal__close")]);
 
   const openMenuButton = this.document.querySelector(".menu-button");
   const mobileMenu = this.document.querySelector(".header-mobile");
   const closeMenuButton = this.document.querySelector(".header-mobile__close");
 
-  openMenuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("header-mobile--visible");
+  openMenuButton.addEventListener("click", () => {
+    mobileMenu.classList.add("header-mobile--visible");
   });
 
-  closeMenuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("header-mobile--visible");
+  closeMenuButton.addEventListener("click", () => {
+    mobileMenu.classList.remove("header-mobile--visible");
   });
 });
