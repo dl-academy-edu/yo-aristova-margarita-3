d07d8f42d8bfd4c0ba7a10dd6d924101e459cb46
rita-aris <83461001+rita-aris@users.noreply.github.com>
2023-01-12 12:26:47 +0400

    added functionality for mobile menu

--- source/js/mobileMenu.js
+++ source/js/mobileMenu.js
@@ -2,6 +2,12 @@
   const mobileMenu = document.querySelector(".header-mobile");
   const openMenuButton = document.querySelector(".menu-button");
   const closeMenuButton = mobileMenu.querySelector(".header-mobile__close");
+  const signInModal = document.querySelector(".sign-in");
+  const signInOpenButton = mobileMenu.querySelector(".nav__link--sign-js");
+  const registerModal = document.querySelector(".register");
+  const registerOpenButton = mobileMenu.querySelector(
+    ".nav__link--register-js"
+  );
 
   openMenuButton.addEventListener("click", () => {
     mobileMenu.classList.add("header-mobile--visible");
@@ -9,5 +15,19 @@
 
   closeMenuButton.addEventListener("click", () => {
     mobileMenu.classList.remove("header-mobile--visible");
+  });
+
+  signInOpenButton.addEventListener("click", () => {
+    const firstInput = signInModal.querySelector(".form__input");
+    mobileMenu.classList.remove("header-mobile--visible");
+    interactiveModal(signInModal);
+    firstInput.focus();
+  });
+
+  registerOpenButton.addEventListener("click", () => {
+    const firstInput = registerModal.querySelector(".form__input");
+    mobileMenu.classList.remove("header-mobile--visible");
+    interactiveModal(registerModal);
+    firstInput.focus();
   });
 })();
