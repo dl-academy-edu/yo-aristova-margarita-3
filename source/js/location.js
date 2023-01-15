d9b7df1cfe8e50c22d7a3e5d89cbdb590659af91
rita-aris <ritaaris@yandex.ru>
2023-01-16 03:46:39 +0400

    some fix in blog.html

--- source/js/location.js
+++ source/js/location.js
@@ -1,7 +1,11 @@
 if (location.search) {
   const filterForm = document.forms.filter;
   const arrayStringParams = location.search.substring(1).split("&");
-  const params = [];
+  const baseUrl = `${location.origin}${location.pathname}`;
+  const arrayCheckedInput = [];
+  const checkedInputsString = "";
+  const params = {};
+  const newUrl = "";
 
   for (let stringParam of arrayStringParams) {
     const param = stringParam.split("=");
@@ -23,4 +27,35 @@ if (location.search) {
       }
     }
   };
+
+  updateInput(filterForm, "views");
+  updateInput(filterForm, "comments");
+  updateInput(filterForm, "show");
+  updateInput(filterForm, "sort");
+
+  filterForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const addCheckedInput = (nameGroupInputs, typeParam) => {
+      for (let checkbox of nameGroupInputs) {
+        if (checkbox.checked) {
+          arrayCheckedInput.push(`${typeParam}=${checkbox.value}`);
+        }
+      }
+    };
+
+    addCheckedInput(e.target.views, "views");
+    addCheckedInput(e.target.comments, "comments");
+    addCheckedInput(e.target.show, "show");
+    addCheckedInput(e.target.sort, "sort");
+
+    for ([index, activeInput] of arrayCheckedInput.entries()) {
+      checkedInputsString += activeInput;
+      if (index != arrayCheckedInput.length - 1) {
+        checkedInputsString += "&";
+      }
+    }
+
+    newUrl = baseUrl + `?${checkedInputsString}`;
+    location = newUrl;
+  });
 }
