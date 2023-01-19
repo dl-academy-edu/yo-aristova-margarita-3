d72467c7e870156f65cba2828af9137d067f7cd2
rita-aris <83461001+rita-aris@users.noreply.github.com>
2023-01-19 14:30:43 +0400

    added new tags block

--- source/js/posts.js
+++ source/js/posts.js
@@ -1,11 +1,85 @@
-let xhr = new XMLHttpRequest();
+const SERVER_URL = "https://academy.directlinedev.com";
+const LIMIT = 9;
+// const loader = document.querySelector(".");
 
-xhr.open("GET", "https://academy.directlinedev.com/api/tags");
+let loaderCount = 0;
 
-xhr.onload = () => {
-  console.log(`Загружено ${xhr.status} и ${xhr.response}`);
+const showLoader = () => {
+  loaderCount++;
+  loader.classList.remove("hidden");
 };
 
-xhr.error = () => {
-  console.log(`Ошибка ${xhr.status}`);
+const hideLoader = () => {
+  loaderCount--;
+  if (loaderCount <= 0) {
+    loader.classList.add("hidden");
+    loaderCount = 0;
+  }
 };
+
+const createTag = ({ id, color }) => {
+  color = color.substring(1, 7);
+  return `
+  <div>
+  <input name="tags" type="checkbox" id="tags-${id}" value="${id}" class="checkbox checkbox--${color}" />
+  <label for="tags-${id}"></label>
+  </div>`;
+};
+
+// (function () {
+//   const filterForm = document.forms.filter;
+//   filterForm.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     let data = {
+//       page: 0,
+//     };
+//     data.tags = [...filterForm.elements.tags];
+//     .filter(checkbox => checkbox.checked);
+//     .map(checkbox => checkbox.value);
+//     data.sort = ([...filterForm.elements.sort]
+//       .find(radio => radio.checked) || {value: null}).value;
+//       getData(data);
+//       setSearchParams(data);
+
+//   });
+// })();
+
+(function () {
+  const filterForm = document.forms.filter;
+  filterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let data = {
+      page: 0,
+    };
+    // data.name = filterForm.elements.name.value;
+    data.sort = (
+      [...filterForm.elements.sort].find((radio) => radio.checked) || {
+        value: null,
+      }
+    ).value;
+  });
+
+  let xhr = new XMLHttpRequest();
+
+  // showLoader();
+  xhr.open("GET", SERVER_URL + "/api/tags");
+  xhr.send();
+
+  xhr.onload = () => {
+    const tags = JSON.parse(xhr.response).data;
+    console.log(tags);
+    const tagsBox = document.querySelector(".tags--js");
+    tags.forEach((tag) => {
+      tagHTML = createTag(tag);
+      tagsBox.insertAdjacentHTML("beforeend", tagHTML);
+    });
+    // const params = getParamsFromLocation();
+    // setDataToFilter(params);
+    // getData(params);
+    // hideLoader();
+  };
+
+  xhr.error = () => {
+    console.log(`Ошибка ${xhr.status}`);
+  };
+})();
